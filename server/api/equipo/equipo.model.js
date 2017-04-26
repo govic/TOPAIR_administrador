334da8cb6ebc22eaf79dad2ab79c1d1c3518d7e4
hfuentes <hectorfuentesg@gmail.com>
2017-04-25 22:21:12 -0400

    cambios en modelo equipo

--- server/api/equipo/equipo.model.js
+++ server/api/equipo/equipo.model.js
@@ -5,22 +5,37 @@ var deepPopulate = require('mongoose-deep-populate')(mongoose);
 var Schema = mongoose.Schema;
 
 var EquipoSchema = new Schema({
-  nombre: String,
-  es_activo: {
-    type: Boolean,
-    default: true
-  },
-  serie: String,
-  marca: String,
-  capacidad: String,
-  modelo: String,
-  antiguedad: String,
-  tipo: { type: Schema.Types.ObjectId, ref: 'TipoEquipo' }
+    nombre: String,
+    es_activo: {
+        type: Boolean,
+        default: true
+    },
+    serie: String,
+    marca: String,
+    capacidad: {
+        type: String,
+        default: ''
+    },
+    modelo: String,
+    antiguedad: String,
+    tipo: {
+        type: Schema.Types.ObjectId,
+        ref: 'TipoEquipo'
+    },
+    year: {
+        type: Number,
+        min: 1970,
+        max: 2099
+    },
+    descripcion: {
+        type: String,
+        default: ''
+    }
 }, {
-  timestamps: {
-    createdAt: 'fecha_creacion',
-    updatedAt: 'fecha_modificacion'
-  }
+    timestamps: {
+        createdAt: 'fecha_creacion',
+        updatedAt: 'fecha_modificacion'
+    }
 });
 
 EquipoSchema.plugin(deepPopulate);

6d5ffe1eb8ae5204230f8f23f12ee4121b17ea03
Hector Fuentes <hectorfuentesg@gmail.com>
2017-05-16 00:38:29 -0400

    add dist

---
+++ dist/server/config/environment/development.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    //uri: 'mongodb://localhost/pcmadministrador-dev'
+    uri: 'mongodb://admin:123456a@ds143211.mlab.com:43211/heroku_972jdrx1'
+  },
+  sequelize: {
+    uri: 'sqlite://',
+    options: {
+      logging: false,
+      storage: 'dev.sqlite',
+      define: {
+        timestamps: false
+      }
+    }
+  },
+  seedDB: true
+};
